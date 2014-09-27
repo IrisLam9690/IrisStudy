a36078346770cd2738b114568af9b61093fec8b0
IrisLam9690 <739701015@qq.com>
2014-09-27 19:32:09 +0800

    Create init.php

---
+++ html-and-css/init.php
@@ -0,0 +1,9 @@
+<?php
+
+define("SAE_MYSQL_HOST_M","localhost");
+define("SAE_MYSQL_PORT","");
+define("SAE_MYSQL_USER","root");
+define("SAE_MYSQL_PASS","");
+define("SAE_MYSQL_DB","dbrecipes" );
+
+?>
